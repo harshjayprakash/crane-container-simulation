2a30c278d9f7b0cf9a33d40748db7f49199c66c8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-21 19:15:53 +0000

    docs: add code documentation

--- Problem.pas
+++ Problem.pas
@@ -1,3 +1,37 @@
+{ Program: DockingProblem
+  Purpose: A simple CLI application to calculate the heuristics and cost 
+           from the initial state.
+  Author: Harsh Jayprakash.
+  Date: January 2023 (modified March 2025 to add documentation)
+  License: MIT.
+  Description:
+    This program demonstrated my ability to calculate the cost and heuristics as part of 
+    my Artificial Intelligence Module at the University of Winchester studying Computer 
+    Science. The task involved a scenario where there were a number of loading bays, 
+    containers and a crane to move a container between each bay. I chose the initial state 
+    as:
+
+            (!)
+
+
+    [5]     [4]
+    [1] [2] [3]
+     A   B   C   D
+    
+    The goal could be anything as long as the containers 1, 2 and 3 were numerically 
+    organised at a single loading bay. I chose the following goal state:
+    
+    [3]
+    [2] [5]
+    [1] [4]
+     A   B   C   D
+
+    I needed to work out the best possible route to reach the goal state from the initial
+    state, with the least cost using a searching algorithm (such as A*). Honestly, I 
+    struggled with this assignment. This program was a way for me to calculate the 
+    heuristics and cost by adding a single line of code to indicate a movement.
+}
+
 program DockingProblem;
 
 {$mode objfpc}
@@ -7,52 +41,114 @@ program DockingProblem;
 uses SysUtils;
 
 const
-    // Limits
+    { Denotes the number of containers in the scenario. }
     CONTAINER_COUNT = 5;
+
+    { Denotes the number of loading bays available in the scenario. }
     LOADING_BAY_COUNT = 4;
+
+    { Denotes the maximum number of containers that could be stacked on one loading bay }
     CONTAINER_STACK_LIMIT = 3;
+
+    { Denotes the maximum number of paths that can be taken. This was to set the upper 
+      limit of the path array.
+    }
     PATH_ALLOC_LIMIT = 50;
 
-    // Time In Minutes
+    { Denotes the time taken for the crane to move between loading bays. }
     LOADING_BAY_TIME = 3;
+
+    { Denotes the maximum time taken for the crane to pick up a container from a loading 
+      bay.
+    }
     MAX_PICKUP_TIME = 4;
+
+    { Denotes the time taken for the crane to drop a container onto a loading bay. }
     DROP_TIME = 2;
 
 type
-    TStackPosition = (Unassigned = -1, Bottom = 0, Middle = 1, Top = 2);
+    { Denotes the what stack position of the loading bay the container 
+      is at. Below shows a text-based diagram:
 
+        [ ]   <-- Top    (Idx 2)
+        [ ]   <-- Middle (Idx 1)
+        [ ]   <-- Bottom (Idx 0)
+      Loading Bay X
+    }
+    TStackPosition = (
+        { A stack position that is unset or held by the crane. }
+        Unassigned = -1, 
+        { At the bottom position of the loading bay stack. }
+        Bottom = 0, 
+        { At the middle position of the loading bay stack. }
+        Middle = 1, 
+        { At the top position of the loading bay stack. }
+        Top = 2
+    );
+
+    { Denotes a single container entity. }
     TContainer = record
+        { The container id. }
         Identifer: Integer;
+        { The loading bay the container is in. }
         InBay: Char;
+        { The stack position in the specified loading bay. }
         BayPosition: TStackPosition;
+        { If the container is picked up by the crane. }
         PickUpByCrane: Boolean;
     end;
 
+    { Denotes a single loading bay. }
     TLoadingBay = record
+        { The loading bay id. }
         Identifer: Char;
+        { The stack of currently held containers (uses container id). }
         Stack: array [0..CONTAINER_STACK_LIMIT] of Integer;
+        { The current number of containers. }
         ContainerCount: Integer;
     end;
 
+    { Denotes the crane. }
     TCrane = record
+        { The loading bay the crane is over. }
         AtBay: Char;
+        { The id of the container the crane is holding. }
         Holding: Integer;
     end;
 
+    { Denotes the move path. Variables prefixed with 'L' to avoid language 
+      keyword conflicts.
+    }
     TPath = record
+        { The source loading bay. }
         LFrom: Char;
+        { The destination loading bay. }
         LTo: Char;
     end;
 
+    { Denotes the whole environment of the scenario. }
     TEnvironment = record
+        { The loading bays. }
         LoadingBays: array [0..LOADING_BAY_COUNT] of TLoadingBay;
+        { The containers. }
         Containers: array [0..CONTAINER_COUNT] of TContainer;
+        { All paths taken. }
         Paths: array [0..PATH_ALLOC_LIMIT] of TPath;
+        { Number of paths. }
         PathCount: Integer;
+        { The crane. }
         Crane: TCrane;
+        { The total cost of moves taken. }
         TotalCost: Integer;
     end;
 
+
+{ Procedure: LoadingBayInit
+  Purpose: Initialises the given loading bay.
+  Parameters:
+    [out] LoadingBay (TLoadingBay) - The loading bay to be initialised.
+    [in]  Identifier (Char)        - The loading bay id.
+}
 procedure LoadingBayInit(var LoadingBay: TLoadingBay; Identifer: Char);
 var Index: Integer;
 begin
@@ -62,18 +158,34 @@ begin
         LoadingBay.Stack[Index] := 0;
 end;
 
+{ Procedure: CraneInit
+  Purpose: Initialises the crane.
+  Parameters:
+    [out] Crane (TCrane) - The crane to be initialised.
+}
 procedure CraneInit(var Crane: TCrane);
 begin
     Crane.AtBay := '0';
     Crane.Holding := 0;
 end;
 
+{ Procedure: ContainerInit
+  Purpose: Initialises a container.
+  Parameters:
+    [out] Container  (TContainer) - The container to be initialised.
+    [in]  Identifier (Integer)    - The container id.
+}
 procedure ContainerInit(var Container: TContainer; Identifer: Integer);
 begin
     Container.Identifer := Identifer;
     Container.BayPosition := TStackPosition.Unassigned;
 end;
 
+{ Procedure: PathsInit
+  Purpose: Initialise the paths array.
+  Paramters:
+    [out] Paths (Array of TPath) - The path array to be initialised.
+}
 procedure PathsInit(var Paths: array of TPath);
 var Index: Integer;
 begin
@@ -84,11 +196,26 @@ begin
     end;
 end;
 
+{ Procedure: CraneAt
+  Purpose: Sets the position of the crane.
+  Parameters:
+    [out] Crane      (TCrane)      - The crane.
+    [in]  LoadingBay (TLoadingBay) - The loading bay the crane should move 
+                                     to.
+}
 procedure CraneAt(var Crane: TCrane; LoadingBay: TLoadingBay);
 begin
     Crane.AtBay := LoadingBay.Identifer;
 end;
 
+{ Procedure: ContainerAt
+  Purpose: Sets the position of the container.
+  Parameters:
+    [out] Container  (TContainer)     - The container.
+    [out] LoadingBay (TLoadingBay)    - The loading bay the container should be in.
+    [in]  StackIndex (TStackPosition) - The position of the container on the loading bay 
+                                        stack.
+}
 procedure ContainerAt(
     var Container: TContainer; var LoadingBay: TLoadingBay; 
     StackIndex: TStackPosition);
@@ -100,6 +227,13 @@ begin
     LoadingBay.Stack[Integer(StackIndex)] := Container.Identifer;
 end;
 
+{ Procedure: CraneHold
+  Purpose: Sets the container the crane is holding.
+  Parameters:
+    [out] Crane      (TCrane)      - The crane.
+    [out] Container  (TContainer)  - The container the crane will hold.
+    [out] LoadingBay (TLoadingBay) - The loading bay the container is in before.
+}
 procedure CraneHold(
     var Crane: TCrane; var Container: TContainer; var LoadingBay: TLoadingBay);
 begin
@@ -109,6 +243,14 @@ begin
     Dec(LoadingBay.ContainerCount, 1);
 end;
 
+{ Procedure: Path
+  Purpose: Adds the path to the stored array.
+  Parameters:
+    [out] Paths          (Array of TPath) - The array of paths to be updated.
+    [out] PathCount      (Integer)        - The path count to be updated.
+    [in]  LoadingBayFrom (TLoadingBay)    - The path from the specified loading bay.
+    [in]  LoadingBayTo   (TloadingBay)    - The path to the specified loading bay.
+}
 procedure Path(
     var Paths: array of TPath; var PathCount: Integer; 
     LoadingBayFrom, LoadingBayTo: TLoadingBay);
@@ -118,6 +260,14 @@ begin
     Inc(PathCount, 1);
 end;
 
+{ Function: MoveCost
+  Purpose: Calculates the cost of the crane moving between loading bays.
+  Parameters:
+    [in] LoadingBayFrom (TLoadingBay) - The loading bay to move from.
+    [in] LoadingBayTo   (TLoadingBay) - The loading bay to move to.
+  Returns:
+    Result (Integer) - The move cost.
+}
 function MoveCost(LoadingBayFrom, LoadingBayTo: TLoadingBay): Integer;
 var Distance: Integer;
 begin
@@ -125,14 +275,37 @@ begin
     MoveCost := (LOADING_BAY_TIME * Distance);
 end;
 
+{ Function: PickUpCost
+  Purpose: Calculates the cost of the picking up a container.
+  Parameters:
+    [in] Pos (TStackPosition) - The position of the container in a loading bay.
+  Returns:
+    Result (Integer) - The pick up cost.
+}
 function PickUpCost(Pos: TStackPosition): Integer;
 begin
     PickUpCost := MAX_PICKUP_TIME - Integer(Pos);
 end;
 
+{ Function: DropCost
+  Purpose: Returns the cost of dropping a container.
+  Parameters:
+    N/A
+  Returns:
+    Result (Integer) - The drop cost.
+}
 function DropCost(): Integer;
 begin DropCost := DROP_TIME end;
 
+{ Function: Move
+  Purpose: Moves the crane from loading bay to another.
+  Parameters:
+    [out] Crane          (TCrane)      - The crane.
+    [in]  LoadingBayFrom (TLoadingBay) - The loading bay the crane is over.
+    [in]  LoadingBayTo   (TLoadingBay) - The loading bay the crane should move to.
+  Returns:
+    Result (Integer) - The move cost.
+}
 function Move(
     var Crane: TCrane; LoadingBayFrom, LoadingBayTo: TLoadingBay): Integer;
 begin
@@ -143,6 +316,15 @@ begin
     Move := MoveCost(LoadingBayFrom, LoadingBayTo);
 end;
 
+{ Function: PickUp
+  Purpose: The crane picks up a container.
+  Parameters:
+    [out] Crane      (TCrane)              - The crane.
+    [out] LoadingBay (TLoadingBay)         - The loading bay the crane is over.
+    [out] Containers (Array of TContainer) - The array of all containers.
+  Returns:
+    Result (Integer) - The pickup cost.
+}
 function PickUp(
     var Crane: TCrane; var LoadingBay: TLoadingBay;
     var Containers: array of TContainer): Integer;
@@ -160,6 +342,15 @@ begin
     Containers[Crane.Holding-1].InBay := '0';
 end;
 
+{ Function: Drop
+  Purpose: The crane drops the container it is holding.
+  Parameters:
+    [out] Crane      (TCrane)              - The crane.
+    [out] LoadingBay (TLoadingBay)         - The loading bay the crane is over.
+    [out] Containers (Array of TContainer) - The array of all containers.
+  Returns:
+    Result (Integer) - The drop cost.
+}
 function Drop(
     var Crane: TCrane; var LoadingBay: TLoadingBay;
     var Containers: array of TContainer): Integer;
@@ -177,11 +368,25 @@ begin
     Drop := DropCost();
 end;
 
+{ Function: HeurBayDistance
+  Purpose: Calculates the heuristic - distance between bays.
+  Parameters:
+    [in] Bay            (Char) - The target loading bay.
+    [in] ContainerInBay (Char) - The current loading bay.
+}
 function HeurBayDistance(Bay: Char; ContainerInBay: Char): Integer;
 begin
     HeurBayDistance := Abs(3 * (Ord(Bay) - Ord(ContainerInBay)));
 end;
 
+{ Function: HeurIndexDistance
+  Purpose: Calculates the heuristic - distance between indexes
+  Parameters:
+    [in] Container (TContainer)     - The container.
+    [in] Bay       (Char)           - The target loading bay.
+    [in] Position  (TStackPosition) - The target stack position of the container.
+    [in] Crane     (TCrane)         - The crane.
+}
 function HeurIndexDistance(
     Container: TContainer; Bay: Char; Position: TStackPosition;
     Crane: TCrane): Integer;
@@ -204,6 +409,11 @@ begin
     end;
 end;
 
+{ Function: Heur
+  Purpose: Calculates the heuristic
+  Parameters:
+    [in] Env (TEnvironment) - The environment.
+}
 function Heur(Env: TEnvironment): Integer;
 var 
     Index: Integer;
@@ -233,6 +443,11 @@ begin
     Inc(Heur, (Dist[0] + Dist[1] + Dist[2] + Dist[3] + Dist[4]));
 end;
 
+{ Procedure: DisplayGraphic
+  Purpose: Display the environment as a text based graphic.
+  Paramters:
+    [in] Env (TEnvironment) - The whole environment.
+}
 procedure DisplayGraphic(Env: TEnvironment);
 var Index, StackIndex, CraneIndex: Integer;
 begin
@@ -330,7 +545,7 @@ begin
     Assert(DropCost() = 2);
     TotalCost^ := 0;
 
-    // Plan.   
+    // Plan - The steps to get to the goal state.
     Inc(TotalCost^, Move(Crane^, LoadingBayD^, LoadingBayC^));
     Inc(TotalCost^, Move(Crane^, LoadingBayC^, LoadingBayB^));
     Inc(TotalCost^, Move(Crane^, LoadingBayB^, LoadingBayA^));
